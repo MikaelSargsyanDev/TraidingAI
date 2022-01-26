662e6a8c197cb1b2e3e60b12e3da7d8041f06bb6
Maher Najar <maher.najar@e2vr.io>
2022-01-26 01:45:05 +0100

    port => PORT

--- app.js
+++ app.js
@@ -38,6 +38,6 @@ app.get('/buyStock/:symbol/:quantity', async (req, res) => {
   res.send({ order });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Listening on port ' + port));
